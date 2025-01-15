6805eb4823cf7647a102f8672681cf9b1c3d7bc2
Bart Massey <bart.massey@gmail.com>
2025-01-14 18:54:31 -0800

    added tuple struct example

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-pub type Value = u32;
 
 /// Returns a maximum value from the slice `a`, or
 /// `None` if `a` is empty.
-pub fn max(a: &[Value]) -> Option<Value> {
+pub fn max<T: PartialOrd + Copy>(a: &[T]) -> Option<T> {
     if a.is_empty() {
         return None;
     }
     let mut m = a[0];
-    for x in &a[1..] {
-        if *x > m {
-            m = *x;
+    for &x in &a[1..] {
+        if x > m {
+            m = x;
         }
     }
     Some(m)
@@ -17,7 +16,7 @@ pub fn max(a: &[Value]) -> Option<Value> {
 
 /// Returns a minimum value from the slice `a`, or
 /// `None` if `a` is empty.
-pub fn min(a: &[Value]) -> Option<Value> {
+pub fn min<T: PartialOrd + Copy>(a: &[T]) -> Option<T> {
     if a.is_empty() {
         return None;
     }
@@ -30,19 +29,8 @@ pub fn min(a: &[Value]) -> Option<Value> {
     Some(m)
 }
 
-/*
-struct S {
-    x: u32,
-    y: u32,
-    z: u32,
-}
-
-pub fn f({x, y, ...}: S) {
-}
-*/
-
 #[test]
 fn test_max() {
-    assert_eq!(Some(3), max(&[1, 3, 2]));
-    assert_eq!(None, max(&[]));
+    assert_eq!(Some(3u32), max(&[1, 3, 2]));
+    assert_eq!(None, max::<u32>(&[]));
 }
